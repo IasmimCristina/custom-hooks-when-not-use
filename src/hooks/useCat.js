2672649608e1232bad7b866f874207a1c4be9b29
IasmimCristina <iaasmimcristinaa@gmail.com>
2024-09-25 20:01:06 -0300

    feat: introduction examples added.

--- src/hooks/useCat.js
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react';
-import { CatContext } from '../context/CatContext';
-
-const useCat = () => {
-  const { cats, addCat } = useContext(CatContext);
-  return { cats, addCat };
-};
-
-export default useCat;
