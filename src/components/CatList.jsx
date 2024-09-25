8525f011a152124aa1fd825abec1c3a9d4067f97
IasmimCristina <iaasmimcristinaa@gmail.com>
2024-09-25 17:07:28 -0300

    feat: cat id and small list.

--- src/components/CatList.jsx
+++ src/components/CatList.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
-
 import useCat from '../hooks/useCat';
 import CatCard from './CatCard';
-
 
 const CatList = () => {
   const { cats } = useCat();
 
   return (
     <div className="cat-list">
-      {cats.map((cat, index) => (
-        <CatCard key={index} breed={cat.breed} image={cat.image} country={cat.country} />
+      {cats.map((cat) => (
+        <CatCard key={cat.id} breed={cat.breed} image={cat.image} country={cat.country} />
       ))}
     </div>
   );
