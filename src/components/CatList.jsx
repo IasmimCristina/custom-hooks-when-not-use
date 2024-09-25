b9b1dcf6d4d3f2588b06be79a2b29d8cefabc519
IasmimCristina <iaasmimcristinaa@gmail.com>
2024-09-25 20:03:28 -0300

    feat: rnaodm

--- src/components/CatList.jsx
+++ src/components/CatList.jsx
@@ -1,25 +1,10 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect } from "react";
 import useCatList from "../hooks/useCatList";
 import CatCard from "./CatCard";
-import bengalImage from "../assets/images/bengal.avif";
-import russianBlueImage from "../assets/images/russian_blue.avif";
 
 const CatList = () => {
-  const { cats, addCat } = useCatList();
-  const hasAddedCats = useRef(false);
-  // useRef to control if the cats are already added
+  const { cats } = useCatList();
 
-  useEffect(() => {
-    if (!hasAddedCats.current) {
-      addCat({ breed: "Bengal", image: bengalImage, country: "USA" });
-      addCat({
-        breed: "Russian Blue",
-        image: russianBlueImage,
-        country: "Russia",
-      });
-      hasAddedCats.current = true;
-    }
-  }, [addCat]);
   return (
     <div className="cat-list">
       <h2>First list</h2>
@@ -35,4 +20,4 @@ const CatList = () => {
   );
 };
 
-export { CatList, TotallyDifferentCatList };
+export default CatList;
