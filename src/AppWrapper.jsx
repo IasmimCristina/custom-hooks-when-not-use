2ada8566a09be7a95794a351a7fb53a2e3743dc0
IasmimCristina <iaasmimcristinaa@gmail.com>
2024-10-01 14:50:46 -0300

    style: app wrapper full height.

--- src/AppWrapper.jsx
+++ src/AppWrapper.jsx
@@ -1,16 +1,10 @@
-// src/components/AppWrapper.jsx
-import React, { useContext } from 'react';
-import { ThemeContext } from './context/ThemeContext';
-
+import React, { useContext } from "react";
+import { ThemeContext } from "./context/ThemeContext";
 
 const AppWrapper = ({ children }) => {
   const { theme } = useContext(ThemeContext);
 
-  return (
-    <div className={theme}>
-      {children}
-    </div>
-  );
+  return <div className={`app-wrapper ${theme}`}>{children}</div>;
 };
 
 export default AppWrapper;
